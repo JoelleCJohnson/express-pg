73d3bf4753a0d9df65956a4edf3c2f61b379926e
Joelle  Johnson <joellej221@gmail.com>
2023-10-18 15:47:16 -0400

    not sure if i saved before last push

--- index.js
+++ index.js
@@ -12,11 +12,6 @@ app.get("/", (req, res) => {
     res.send("Hello!")
 })
 
-app.post("/", (req, res) => {
-    const newCustomer = req.body
-    
-})
-
 app.listen(PORT, () => {
     console.log(`Listening on http://localhost:${PORT}...`)
 })
