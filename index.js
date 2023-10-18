8ebd0d8506ea5613c4a6f7e837dcdee523d16406
Joelle  Johnson <joellej221@gmail.com>
2023-10-18 16:53:44 -0400

    error in my addMenuItem function, need to work on it when I get home

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import express from 'express'
 import cors from 'cors'
 import { client } from './src/connectDB.js'
-import { getAllMenuItems } from './src/menu.js'
+import { addMenuItem, getAllMenuItems } from './src/menu.js'
 
 const app = express()
 const PORT = 3000
@@ -12,6 +12,8 @@ app.use(cors())
 app.get("/", (req, res) => {
     res.send("Hello!")
 })
+
+app.post("/menu", addMenuItem)
 
 app.get("/menu", getAllMenuItems)
 
--- src/menu.js
+++ src/menu.js
@@ -4,3 +4,17 @@ export async function getAllMenuItems(req, res){
     const result = await client.query('SELECT * FROM menu')
     res.send(result.rows)
 }
+
+export async function addMenuItem(req, res){
+    const body = req.body
+    // const id = body.id
+    // const name = body.name
+    // const description = body.description
+    // const price = body.price
+    const { id, name, description, price } = body //destructuring, same as doing lines 10 - 13 but much shorter/ easier
+
+    const result = await client.query(`INSERT INTO menu (id, name, description, price)
+    VALUES(${id}, '${name}', '${description}', ${price})`)
+
+    res.send(result)
+}
